03f5bc45bd2bd3099b5661d7a2000f98ba0999de
Cristian Andres Fernandez Bermudez <crafernandezbe@unal.edu.co>
2021-06-13 17:55:01 -0500

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Illo soluta itaque cum dolores. Quibusdam
+        tenetur explicabo autem ipsam, vitae eius magni,
+        tempora, sed labore exercitationem iusto
+        voluptatibus ullam necessitatibus ratione.
+      </p>
+    </section>
+  );
+};
+
+export default About;
